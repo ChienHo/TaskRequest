e5b33794ae7be685f0190707e0153ff9bf4c19af
ChienHo <Chienhos2@gmail.com>
2019-04-24 02:20:30 +0700

    add identity

---
+++ TaskRequest/TaskRequest.Prsentation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskRequest.Presentation.MvcApplication" Language="C#" %>
---
+++ TaskRequest/TaskRequest.Prsentation/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace TaskRequest.Presentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
